5977a136f803da88c9245e31c5dc4aec504b4098
sntmailer <suntech.mailer.76@proton.me>
2026-04-04 22:57:43 -0400

    build(deps): build some important dependencies.

--- cgo/libs.go
+++ cgo/libs.go
@@ -1,7 +1,7 @@
 package cgo
 
 /*
-#cgo LDFLAGS: -L${SRCDIR}/.. -lfilcrypto -lmultipqcsig
+#cgo LDFLAGS: -L${SRCDIR}/.. -lfilcrypto 
 #cgo pkg-config: ${SRCDIR}/../filcrypto.pc
 #include "../filcrypto.h"
 #include <stdlib.h>
